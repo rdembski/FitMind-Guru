627d4d73f01088d7d52574264f463cdf26b69967
rdembski <149225137+rdembski@users.noreply.github.com>
2024-03-08 22:06:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ document.getElementById('food-analysis-form').addEventListener('submit', functio
     // Tutaj możesz dodać logikę przetwarzającą przesłane zdjęcie
     console.log('Analiza zdjęcia produktu...');
 });
+document.getElementById('food-analysis-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var fileInput = document.getElementById('foodImage');
+    var filePath = fileInput.value;
+    var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+
+    if (!allowedExtensions.exec(filePath)) {
+        alert('Proszę przesłać plik w formacie .jpeg/.jpg/.png/.gif.');
+        fileInput.value = '';
+        return false;
+    } else {
+        // Przetwarzaj zdjęcie...
+        console.log('Analiza zdjęcia produktu...');
+    }
+});
