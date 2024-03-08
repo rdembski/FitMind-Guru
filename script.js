cc79f8c451ec55d44f54863bb02df83899279a04
rdembski <149225137+rdembski@users.noreply.github.com>
2024-03-08 22:02:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Welcome to FitMind Guru");
+document.getElementById('food-analysis-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Tutaj możesz dodać logikę przetwarzającą przesłane zdjęcie
+    console.log('Analiza zdjęcia produktu...');
+});
